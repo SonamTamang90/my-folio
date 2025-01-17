084b1c2cba9a7ea8791a792b0170230cff062f46
stamang <stamang.mrbhutan90@gmail.com>
2025-01-17 21:11:32 +0800

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+        bebas: ["Bebas Neue", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
